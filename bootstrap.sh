d54dffa32fc2c295eb5da0df946b91276fb39d59
Stuart Grimshaw <stuart@localphone.com>
2013-05-09 10:51:59 +0100

    Added bootstrap script to install the required puppet modules.

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+puppet module install puppetlabs-apache
+puppet module install puppetlabs-apt
+puppet module install puppetlabs-firewall
+puppet module install puppetlabs-mysql
+puppet module install puppetlabs-stdlib
